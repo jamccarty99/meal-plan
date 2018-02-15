f6333ab69742b3d84808e632d28e699d1c96733b
Jorge Rodriguez <rodri0315@gmail.com>
2018-02-15 15:09:05 -0500

    Add components for the meal plan

---
+++ src/components/single-meal.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import MealRecipe from './meal-recipe'
+
+export default class SingleMeal extends Component {
+  render() {
+    return (
+      <div className="container">
+      <h3 className="text-center mb-5">Daily Meal Plan</h3>
+      <h3 className="text-center mb-5">Info</h3>
+        <div className="row">
+          <div className="card-deck">
+          {
+            //map through each meal and render their respective single meal 
+          }
+            <MealRecipe mealProps={props} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
