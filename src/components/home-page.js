4547b251b7531cb05ebfa378c1ba5d6b64538909
Justin McCarty <jamccarty99@gmail.com>
2018-02-14 19:34:21 -0500

    HomePage includes form.

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -3,22 +3,56 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Link } from "react-router-dom";
+import { Field, reduxForm } from "redux-form";
 //import {  } from "../actions";
 
 class HomePage extends Component {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
+
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit() {
+    //TODO: submit form should get converted to an api Get call.
+    //
+    }
 
   render() {
     return (
-      <div>
-        <div className="text-xs-right">
-          <Link className="btn btn-primary" to="/">
-            Create Meal Plan
-          </Link>
-        </div>
-        <h3>Recipes</h3>
-        <ul className="list-group">
-        </ul>
-      </div>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Type of Diet"
+          name="diet"
+          component={this.renderField}
+        />
+        <Field
+          label="Food Exclusions"
+          name="exclude"
+          component={this.renderField}
+        />
+        <Field
+          label="Target Calories"
+          name="calories"
+          component={this.renderField}
+        />
+        <Field
+          label="Day or Week"
+          name="timeframe"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit Search</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
     );
   }
 }
