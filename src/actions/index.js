2bc9bc7320dc640c184a70bf3c7bd44a0855e9c5
Justin McCarty <jamccarty99@gmail.com>
2018-02-15 11:10:04 -0500

    include api key and url in actions index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
-const ROOT_URL = "";
-const API_KEY = "";
+const axios = require('axios');
+const ROOT_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/4632/summary";
+const API_KEY = "M8Na5UOHF8mshxZEgTlYkp39uyb4p1boQEkjsnP4hmi8Gk0DmY"
