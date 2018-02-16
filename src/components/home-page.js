ccdbe7a1e5e3fa40a57180346dd111c9836bee8c
Justin McCarty <jamccarty99@gmail.com>
2018-02-15 21:25:11 -0500

    include header in homepage and update form

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -2,17 +2,18 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './home-page.css'
-//import Header from './components/header'
+import Header from './header'
+
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      diet: '',
+      time: 'Day',
+      diet: 'None',
       exclude: '',
-      calories: '',
-      time: '',
+      calories: ''
     }
 
     this.handleDietChange = this.handleDietChange.bind(this);
@@ -26,6 +27,7 @@ class HomePage extends React.Component {
     this.setState({diet: event.target.value});
   }
   handleExcludeChange(event) {
+    console.log(event.target.value);
     this.setState({exclude: event.target.value});
   }
   handleCaloriesChange(event) {
@@ -37,18 +39,29 @@ class HomePage extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    console.log(this.state.time);
+    console.log(this.state.diet);
+    console.log(this.state.exlude);
+    console.log(this.state.calories);
     alert('Your meal is being planned!');
 
+  }
+
+  onSubmit(values) {
+    this.props.fetchMealPlan(values, () => {
+      this.props.history.push("/");
+    });
   }
 
   render() {
     return (
       <div className="search-form">
+        <Header/>
         <h1>Meal Plan Requests</h1>
         <form onSubmit={this.handleSubmit}>
           <label>
             Day or Week:
-            <select value={this.state.timeframe} onChange={this.handleTimeChange}>
+            <select value={this.state.time} onChange={this.handleTimeChange}>
               <option className="time" value="day">Day</option>
               <option className="time" value="week">Week</option>
             </select>
@@ -56,6 +69,7 @@ class HomePage extends React.Component {
           <label>
             Diet Type:
             <select value={this.state.diet} onChange={this.handleDietChange}>
+              <option className="diet" value="none">None</option>
               <option className="diet" value="vegetarian">Vegetarian</option>
               <option className="diet" value="vegan">Vegan</option>
               <option className="diet" value="glutenFree">Gluten Free</option>
@@ -71,17 +85,16 @@ class HomePage extends React.Component {
           </label>
           <label>
             Food Exclusions:
-            <input type="text" className="exclude" value={this.state.exclude} onChange={this.handleExcludeChange} />
+            <input type="text" placeholder="A comma-separated list of ingredients." className="exclude" value={this.state.exclude} onChange={this.handleExcludeChange} />
           </label>
           <label>
             Target Calories:
-            <input type="number" className="calories" value={this.state.calories} onChange={this.handleCaloriesChange} />
+            <input type="number" placeholder="Your target number of calories."className="calories" value={this.state.calories} onChange={this.handleCaloriesChange} />
           </label>
           <button type="submit" className="btn btn-primary">Create Meal Plan</button>
         </form>
       </div>
     );
-    console.log(this.diet);
   }
 }
 
