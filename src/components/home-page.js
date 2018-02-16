a9f20ef637206e9fa7ca24590f3d3ac6a1c0778f
Justin McCarty <jamccarty99@gmail.com>
2018-02-16 14:08:50 -0500

    change css and search form

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -59,45 +59,48 @@ class HomePage extends React.Component {
 
   render() {
     return (
-      <div className="search-form">
-        <h1>Meal Plan Requests</h1>
+      <div className="container search-form">
+        <h1 className="text-center">Meal Plan Requests</h1>
         <form className="form-horizontal" onSubmit={this.handleSubmit}>
           <div className="form-group row">
-            <label className="col-sm-2 col-form-label">
-              Day/Week:
-              <select className="form-control" value={this.state.time} onChange={this.handleTimeChange}>
-                <option className="time" value="day">Day</option>
-                <option className="time" value="week">Week</option>
-              </select>
-            </label>
+            <label className="col-sm-2 col-form-label">Day/Week:</label>
+              <div className="col-sm-10">
+                <select className="form-control mt-1" value={this.state.time} onChange={this.handleTimeChange}>
+                  <option className="time" value="day">Day</option>
+                  <option className="time" value="week">Week</option>
+                </select>
+              </div>
+            <label className="col-sm-2 col-form-label">Diet Type:</label>
+              <div className="col-sm-10">
+                <select className="form-control mt-3 mb-2" value={this.state.diet} onChange={this.handleDietChange}>
+                  <option className="diet" value="none">None</option>
+                  <option className="diet" value="vegetarian">Vegetarian</option>
+                  <option className="diet" value="vegan">Vegan</option>
+                  <option className="diet" value="glutenFree">Gluten Free</option>
+                  <option className="diet" value="dairyFree">Dairy Free</option>
+                  <option className="diet" value="veryHealthy">Very Healthy</option>
+                  <option className="diet" value="cheap">Cheap</option>
+                  <option className="diet" value="veryPopular">Very Popular</option>
+                  <option className="diet" value="sustainable">Sustainable</option>
+                  <option className="diet" value="lowFodmap">Low Fodmap</option>
+                  <option className="diet" value="ketogenic">Ketogenic</option>
+                  <option className="diet" value="whole30">Whole 30</option>
+                </select>
+              </div>
+            <label className="col-sm-2 col-form-label">Food Exclusions:</label>
+              <div className="col-sm-10">
+                <input type="text" placeholder="A comma-separated list of ingredients." className="exclude" value={this.state.exclude} onChange={this.handleExcludeChange} />
+              </div>
+            <label className="col-sm-2 col-form-label">Target Calories:</label>
+              <div className="col-sm-10">
+                <input type="number" placeholder="Your target number of calories."className="calories" value={this.state.calories} onChange={this.handleCaloriesChange} />
+              </div>
+            <div className="col-sm-offset-2 col-sm-10">
+              <Link to={"/meals"}>
+                <button type="submit" className="btn btn-primary">Create Meal Plan</button>
+              </Link>
+            </div>
           </div>
-          <label className="control-label">
-            Diet Type:
-            <select value={this.state.diet} onChange={this.handleDietChange}>
-              <option className="diet" value="none">None</option>
-              <option className="diet" value="vegetarian">Vegetarian</option>
-              <option className="diet" value="vegan">Vegan</option>
-              <option className="diet" value="glutenFree">Gluten Free</option>
-              <option className="diet" value="dairyFree">Dairy Free</option>
-              <option className="diet" value="veryHealthy">Very Healthy</option>
-              <option className="diet" value="cheap">Cheap</option>
-              <option className="diet" value="veryPopular">Very Popular</option>
-              <option className="diet" value="sustainable">Sustainable</option>
-              <option className="diet" value="lowFodmap">Low Fodmap</option>
-              <option className="diet" value="ketogenic">Ketogenic</option>
-              <option className="diet" value="whole30">Whole 30</option>
-            </select>
-          </label>
-          <label>
-            Food Exclusions:
-            <input type="text" placeholder="A comma-separated list of ingredients." className="exclude" value={this.state.exclude} onChange={this.handleExcludeChange} />
-          </label>
-          <label>
-            Target Calories:
-            <input type="number" placeholder="Your target number of calories."className="calories" value={this.state.calories} onChange={this.handleCaloriesChange} />
-          </label>
-
-          <button type="submit" className="btn btn-primary">Create Meal Plan</button>
         </form>
       </div>
     );
