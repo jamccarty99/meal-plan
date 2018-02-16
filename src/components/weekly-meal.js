a2da0ab39ad52d816dbd5034b4dd290fe32cbf10
Justin McCarty <jamccarty99@gmail.com>
2018-02-16 11:23:53 -0500

    update fontawesome

--- src/components/weekly-meal.js
+++ src/components/weekly-meal.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import SingleMeal from './single-meal'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import {faEdit} from '@fortawesome/fontawesome-pro-regular'
+import {faTrashAlt} from '@fortawesome/fontawesome-pro-regular'
 
 export default class WeekMeal extends Component {
   render() {
